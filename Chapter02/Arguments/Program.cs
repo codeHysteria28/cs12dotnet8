899347cca6850e93063478a02bc00d4c7282252c
Branislav Buna <46035047+codeHysteria28@users.noreply.github.com>
2024-06-25 21:41:06 +0100

    async/await & args

---
+++ Chapter02/Arguments/Program.cs
@@ -0,0 +1,35 @@
+﻿// WriteLine($"There're {args.Length} arguments.");
+
+// foreach(string arg in args){
+//     WriteLine(arg);
+// }
+
+if(args.Length < 3){
+    WriteLine("You must specify two colors and cursor size e.g.");
+    WriteLine("dotnet run red yellow 50");
+    return; // stop running
+}
+
+ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[0], ignoreCase: true
+);
+
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[1], ignoreCase: true
+);
+
+// macOS cannot change the cursor size so it will complain at compile time
+CursorSize = int.Parse(args[2]);
+
+// handling platforms that do not support an API
+try {
+    CursorSize = int.Parse(args[2]);
+}catch(PlatformNotSupportedException) {
+    WriteLine("The current platform does not support chaning the size of the cursor.");
+}
+
+// another way will be to use OperatingSysten class in System namespace
+// or to use conditional compilation statements
+// #define MYSYMBOL;
--- Chapter02/Formating/Program.cs
+++ Chapter02/Formating/Program.cs
@@ -4,7 +4,7 @@
 int numberOfApples = 12;
 decimal pricePerApple = 0.35M;
 
-Console.WriteLine(
+WriteLine(
     format: "{0} apples cost {1:C}",
     arg0: numberOfApples,
     arg1: pricePerApple * numberOfApples    
@@ -17,19 +17,19 @@ string formatted = string.Format(
 );
 
 // format methods can have up to three numbered args
-Console.WriteLine(
+WriteLine(
     "{0} {1} lived in {2}",
     arg0: "Roger", arg1: "Cevung", arg2: "Stockholm"
 );
 
 // four or more parameter values cannot use named args
-Console.WriteLine(
+WriteLine(
     "{0} {1} liven in {2} and worked in the {3} team at {4}.",
     "Roger", "Cevung", "Stockholm", "Education", "Optimizely"
 );
 
 // ? Interpolated string
-Console.WriteLine($"{numberOfApples} apples cost {pricePerApple * numberOfApples:C}");
+WriteLine($"{numberOfApples} apples cost {pricePerApple * numberOfApples:C}");
 
 // Understanding string formats
 // * syntax: { index, [, alignment ] [ : formatString] }
@@ -39,10 +39,28 @@ int applesCount = 1234;
 string bananasText = "Bananas";
 int bananasCount = 56789;
 
-Console.WriteLine();
-Console.WriteLine(format: "{0, -10} {1,6}",
+WriteLine();
+WriteLine(format: "{0, -10} {1,6}",
     arg0: "Name", arg1: "Count");
-Console.WriteLine(format: "{0, -10} {1,6:N0}",
+WriteLine(format: "{0, -10} {1,6:N0}",
     arg0: applesText, arg1: applesCount);
-Console.WriteLine(format: "{0, -10} {1,6:N0}",
+WriteLine(format: "{0, -10} {1,6:N0}",
     arg0: bananasText, arg1: bananasCount);
+
+// * Getting and input from a user
+WriteLine("Type your first name and press ENTER:");
+// ! "question mark = ?" tells the compiler that we're expecting a possible
+// ! null value, so it does not need to tell us
+string? firstName = ReadLine();
+WriteLine("Type your age and press ENTER:");
+// ! exclamation mark is called "null-forgiving operator", it tells the compiler
+// ! that ReadLine will not return null in this case
+string age = ReadLine()!;
+WriteLine($"Hello {firstName}, you look good for {age}."); 
+
+// * Getting key input
+Write("Press any key combination: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+    arg0: key.Key, arg1: key.KeyChar, arg2: key.Modifiers);

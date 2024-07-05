c8621d1fa0f8bf029295ea0e6d5b9df8ff422321
Branislav Buna <46035047+codeHysteria28@users.noreply.github.com>
2024-07-05 19:09:06 +0100

    adding arrays

--- Chapter03/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
+++ Chapter03/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02f6df866c1ce6802dc07ca14df5e0cb1dc81382")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83e4342905cdf013f58d985f2a16e1c31d295ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

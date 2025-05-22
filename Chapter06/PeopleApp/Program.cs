3a421bffd85891830256387b3e0a683b6d1acf7e
codeHysteria28 <graphicglobal67@gmail.com>
2025-05-22 19:47:47 +0100

    ch 6

---
+++ Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Packt.Shared;
+
+Person harry = new()
+{
+    Name = "Harry",
+    Born = new(year: 2001, month: 3, day: 25, hour: 0, minute: 0, second: 0, offset: TimeSpan.Zero)
+};
+
+harry.WriteToConsole();
+
+// Implementing functionality using methods
+Person lamech = new() { Name = "Lamech" };
+Person adah = new() { Name = "Adah" };
+Person zillah = new() { Name = "Zillah" };
